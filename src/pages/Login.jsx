9b8dfca922105bf977ac68ba13e0b308ea73a830
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-13 14:59:29 -0300

    feat: Requisito 2 - form and inputs

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,52 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  state = {
+    userName: '',
+    isDisabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, this.validateFields);
+  };
+
+  validateFields = () => {
+    const { userName } = this.state;
+    const MIN_USERNAME_LENGTH = 3;
+    if (userName.length >= MIN_USERNAME_LENGTH) {
+      this.setState({ isDisabled: false });
+    } else {
+      this.setState({ isDisabled: true });
+    }
+  };
+
   render() {
+    const { isDisabled, userName } = this.state;
     return (
       <div data-testid="page-login">
         <h1>Login</h1>
+        <form>
+          <label>
+            Nome
+            <input
+              name="userName"
+              value={ userName }
+              type="text"
+              data-testid="login-name-input"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            disabled={ isDisabled }
+            data-testid="login-submit-button"
+          >
+            Entrar
+
+          </button>
+        </form>
       </div>
     );
   }
