24bad2dbe77ec052a30cc7eeaba7d1b7e5fe18e4
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-13 14:54:07 -0300

    feat: Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <BrowserRouter>
-          <Route path="/search" component={ Search } />
-          <Route path="/album/:id" component={ Album } />
-          <Route path="/favorites" component={ Favorites } />
-          <Route path="/profile" component={ Profile } />
-          <Route path="/profile/edit" component={ ProfileEdit } />
-          <Route path="/" component={ Login } />
-          <Route component={ NotFound } />
+          <Switch>
+            <Route exact path="/search" component={ Search } />
+            <Route exact path="/album/:id" component={ Album } />
+            <Route exact path="/favorites" component={ Favorites } />
+            <Route exact path="/profile" component={ Profile } />
+            <Route exact path="/profile/edit" component={ ProfileEdit } />
+            <Route exact path="/" component={ Login } />
+            <Route exact component={ NotFound } />
+          </Switch>
         </BrowserRouter>
       </div>
     );
