642ec0e995b734d518850488af8048a33a4be356
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-13 14:46:38 -0300

    feat: Requisito 1 - routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
-    return (<p>My TrybeTunes</p>);
+    return (
+      <div>
+        <BrowserRouter>
+          <Route path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route component={ NotFound } />
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
