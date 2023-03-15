52f8345be06eacc0b4cc11729e2ae8189e298051
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-15 12:11:16 -0300

    feat: requisito 14.5

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 import Loading from '../components/Loading';
-import { getUser } from '../services/userAPI';
+import { getUser, updateUser } from '../services/userAPI';
 
 class ProfileEdit extends Component {
   constructor() {
     super();
 
     this.state = {
-      loading: false,
       nameInput: '',
       imageInput: '',
       emailInput: '',
       descriptionInput: '',
+      loading: false,
+      isDisabled: true,
     };
   }
 
@@ -34,15 +35,38 @@ class ProfileEdit extends Component {
 
   handleChange = ({ target }) => {
     const { name, value } = target;
-    this.setState({ [name]: value });
+    this.setState({ [name]: value }, this.validateFields);
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
   };
 
+  validateFields = () => {
+    const { nameInput, imageInput, emailInput, descriptionInput } = this.state;
+    const states = [nameInput, imageInput, emailInput, descriptionInput];
+    if (states.some((field) => field.length < 1)) {
+      this.setState({ isDisabled: true });
+    } else {
+      this.setState({ isDisabled: false });
+    }
+  };
+
+  handleUpdateUser = async () => {
+    const { nameInput, emailInput, imageInput, descriptionInput } = this.state;
+    this.setState({ loading: true });
+    await updateUser({
+      name: nameInput,
+      email: emailInput,
+      image: imageInput,
+      description: descriptionInput,
+    });
+    this.setState({ loading: false });
+  };
+
   render() {
-    const { loading, nameInput, imageInput, emailInput, descriptionInput } = this.state;
+    const { loading, nameInput, imageInput,
+      emailInput, descriptionInput, isDisabled } = this.state;
     return (
       <div data-testid="page-profile-edit">
         <Header />
@@ -62,7 +86,7 @@ class ProfileEdit extends Component {
           </label>
           <label htmlFor="emailInput">
             <input
-              type="text"
+              type="email"
               name="emailInput"
               value={ emailInput }
               id="emailInput"
@@ -96,6 +120,8 @@ class ProfileEdit extends Component {
           <button
             type="submit"
             data-testid="edit-button-save"
+            disabled={ isDisabled }
+            onClick={ this.handleUpdateUser }
           >
             Salvar
 
