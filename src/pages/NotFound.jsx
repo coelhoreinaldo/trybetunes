41b84af419e07f0d5bb1bfaae80d01ed752288b3
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-17 11:47:13 -0300

    style: not-found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,8 +3,15 @@ import React, { Component } from 'react';
 class NotFound extends Component {
   render() {
     return (
-      <div data-testid="page-not-found">
-        <h1>Page not found</h1>
+      <div className="not-found" data-testid="page-not-found">
+        <h1>
+          Ops!
+        </h1>
+        <h2>
+          A página que você
+          está procurando
+          não foi encontrada.
+        </h2>
       </div>
     );
   }
