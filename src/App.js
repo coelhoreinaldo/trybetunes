a874896c8971261079f33f70ce33b9c0658e0a59
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-19 14:46:53 -0300

    feat: router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
@@ -21,25 +21,23 @@ import './styles/ProfileEdit.css';
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/album/:id" component={ Album } />
-          <Route exact path="/favorites" component={ Favorites } />
-          <Route exact path="/profile" component={ Profile } />
-          <Route exact path="/profile/edit" component={ ProfileEdit } />
-          <Route
-            exact
-            path="/search"
-            component={ Search }
-          />
-          <Route
-            exact
-            path="/"
-            component={ Login }
-          />
-          <Route exact component={ NotFound } />
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <Route exact path="/album/:id" component={ Album } />
+        <Route exact path="/favorites" component={ Favorites } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/profile/edit" component={ ProfileEdit } />
+        <Route
+          exact
+          path="/search"
+          component={ Search }
+        />
+        <Route
+          exact
+          path="/"
+          component={ Login }
+        />
+        <Route exact component={ NotFound } />
+      </Switch>
     );
   }
 }
